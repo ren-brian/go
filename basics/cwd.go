e0529acdb788818ca81602c1b946c53e2f2885b2
reanand <reanand@cisco.com>
2018-08-26 18:31:04 +0530

    init fn example and local scope of if

--- basics/cwd.go
+++ basics/cwd.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-    "os"
-    "log"
+	"log"
+	"os"
 )
 
 func init() {
-    if cwd, err := os.Getwd(); err != nil {
-        log.Fatalf("os.Getwd() failed: %v\n", err)
-    } else {
-        log.Printf("Current working dir: %s\n", cwd)
-    }
+	if cwd, err := os.Getwd(); err != nil {
+		log.Fatalf("os.Getwd() failed: %v\n", err)
+	} else {
+		log.Printf("Current working dir: %s\n", cwd)
+	}
 }
 
 func main() {
-    if host, err := os.Hostname(); err != nil {
-        log.Fatalf("os.Hostname() failed: %v\n", err)
-    } else {
-        log.Printf("Hostname: %s\n", host)
-    }
+	if host, err := os.Hostname(); err != nil {
+		log.Fatalf("os.Hostname() failed: %v\n", err)
+	} else {
+		log.Printf("Hostname: %s\n", host)
+	}
 }
